702e0e5bb37dd2cbb25939ab9c375f6753bc73b0
Fahri MacBook <fahri-can@web.de>
2018-01-08 20:36:50 +0100

    finished model part of the MVP

--- app/src/main/java/com/android/example/cryptocurrencystory/model/Story.java
+++ app/src/main/java/com/android/example/cryptocurrencystory/model/Story.java
@@ -1,11 +1,43 @@
 package com.android.example.cryptocurrencystory.model;
 
+import com.android.example.cryptocurrencystory.R;
+
 public class Story {
 
-    private String[] stories;
+    private Page[] stories;
 
     public void startStories (){
 
-        stories = new String[7];
+        stories = new Page[7];
+
+        stories[0] = new Page(R.drawable.page0,
+                R.string.page0,
+                new Choice(R.string.page1_choice1, 1),
+                new Choice(R.string.page1_choice2, 2));
+
+
+        stories[1] = new Page(R.drawable.page1,
+                R.string.page1,
+                new Choice(R.string.page1_choice1, 3),
+                new Choice(R.string.page1_choice2, 4));
+
+        stories[2] = new Page(R.drawable.page2,
+                R.string.page2,
+                new Choice(R.string.page2_choice1, 4),
+                new Choice(R.string.page2_choice2, 6));
+
+        stories[3] = new Page(R.drawable.page3,
+                R.string.page3,
+                new Choice(R.string.page3_choice1, 4),
+                new Choice(R.string.page3_choice2, 5));
+
+        stories[4] = new Page(R.drawable.page4,
+                R.string.page4,
+                new Choice(R.string.page4_choice1, 5),
+                new Choice(R.string.page4_choice2, 6));
+
+        stories[5] = new Page(R.drawable.page5, R.string.page5);
+
+        stories[6] = new Page(R.drawable.page6, R.string.page6);
     }
 }
--- app/src/main/java/com/android/example/cryptocurrencystory/ui/StoryActivity.java
+++ app/src/main/java/com/android/example/cryptocurrencystory/ui/StoryActivity.java
@@ -2,10 +2,18 @@ package com.android.example.cryptocurrencystory.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.android.example.cryptocurrencystory.R;
+import com.android.example.cryptocurrencystory.model.Choice;
 
 public class StoryActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = StoryActivity.class.getSimpleName();
+    private ImageView image;
+    private String story;
+    private Choice choice1;
+    private Choice choice2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
